437e61406bf41b5a51118b8a063c29cbc0e30059
TOMA <throuquette@hotmail.fr>
2024-07-25 18:20:59 +0200

    add creating posts, loading posts (server-side), trending topics & whotofollow sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import TrendsSidebar from "@/components/TrendsSidebar";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
+
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
   return (
-    <main className="h-[200vh] w-full">
-      <div className="w-full">Front page</div>
+    <main className="w-full min-w-0 flex gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+      <TrendsSidebar />
     </main>
   );
 }
